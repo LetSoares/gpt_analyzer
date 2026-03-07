e85ff5527309d2b7e70a496eadb40f6a936ae8b3
Letícia Soares <gsoares.leticia@gmail.com>
2026-03-07 18:24:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ st.markdown(f"""
     <div style="display: flex; align-items: center; justify-content: center; gap: 8px; margin-top: 16px;">
         <span style="font-family: 'GCartumRegular', serif; font-size: 0.5rem; color: #060638; opacity: 0.7;">
             Criado por
-        </span></br>
+        </br></span>
         <img src="data:image/png;base64,{logo_b64}" style="height: 67px; width: auto;margin-top: 8px" />
     </div>
 """, unsafe_allow_html=True)
