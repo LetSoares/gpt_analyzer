b8ede878c7b9e8f8f33cbb7ea9d132d95f4e8e9b
Letícia Soares <gsoares.leticia@gmail.com>
2026-03-07 18:05:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,9 @@ st.markdown("""
         [data-testid="stTextArea"] textarea {
             background-color: #ffffff !important;
             color: #000000 !important;
-            border: 1px solid #060638 !important;
-            border-radius: 6px !important;
-            box-shadow: 0 4px 15px rgba(231, 42, 28, 0.8) !important;
+            border: 2px solid #060638 !important;
+            border-radius: 8px !important;
+            box-shadow: 0 4px 15px rgba(231, 42, 28, 0.4) !important;
             font-family: 'GCartumRegular', serif !important;
         }
 
